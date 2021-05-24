74075e05189d587812658c9c7f4d69c14d7e9819
HwangJiseob <ghkdwltjq98@naver.com>
2021-05-24 14:54:32 +0900

    add: more svg assets
    add: more svg assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,45 @@
+import { ReactComponent as ArrowRightSVG } from "./arrowRight.svg";
+import { ReactComponent as CartSVG } from "./cart.svg";
 import { ReactComponent as ChatSVG } from "./chat.svg";
 import { ReactComponent as FilesSVG } from "./files.svg";
+import { ReactComponent as FilterSVG } from "./filter.svg";
+import { ReactComponent as HeadphoneSVG } from "./headphone.svg";
+import { ReactComponent as HeartSVG } from "./heart.svg";
 import { ReactComponent as HomeSVG } from "./home.svg";
+import { ReactComponent as LinkCopySVG } from "./linkCopy.svg";
 import { ReactComponent as ManSVG } from "./man.svg";
+import { ReactComponent as MarkerSVG } from "./marker.svg";
+import { ReactComponent as ReviewSVG } from "./review.svg";
 import { ReactComponent as SearchSVG } from "./search.svg";
 
-export { HomeSVG, ChatSVG, FilesSVG, ManSVG, SearchSVG };
+export {
+  ArrowRightSVG,
+  CartSVG,
+  ChatSVG,
+  FilesSVG,
+  FilterSVG,
+  HomeSVG,
+  LinkCopySVG,
+  HeadphoneSVG,
+  HeartSVG,
+  ManSVG,
+  MarkerSVG,
+  ReviewSVG,
+  SearchSVG,
+};
 
-export default { HomeSVG, ChatSVG, FilesSVG, ManSVG, SearchSVG };
+export default {
+  ArrowRightSVG,
+  CartSVG,
+  ChatSVG,
+  FilesSVG,
+  FilterSVG,
+  HomeSVG,
+  LinkCopySVG,
+  HeadphoneSVG,
+  HeartSVG,
+  ManSVG,
+  MarkerSVG,
+  ReviewSVG,
+  SearchSVG,
+};
