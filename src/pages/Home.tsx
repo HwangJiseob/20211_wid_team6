b6e7dd1a14d889dde7e330df49fc5605f20ec7de
HwangJiseob <ghkdwltjq98@naver.com>
2021-05-25 01:35:30 +0900

    update: Home.tsx design
    update: Home.tsx design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,53 +3,39 @@ import styled from "@emotion/styled";
 
 const Home = () => {
   return (
-    <>
+    <Container>
       <Section1>
         <BannerButton>꽃</BannerButton>
         <BannerButton>케이크</BannerButton>
       </Section1>
-      <Section2>
-        <Item>아이템 1</Item>
-        <Item>아이템 2</Item>
-        <Item>아이템 3</Item>
-        <Item>아이템 4</Item>
-        <Item>아이템 5</Item>
-      </Section2>
-    </>
+    </Container>
   );
 };
 
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  width: 100%;
+  height: calc(100vh - 150px); // 일단 하드코딩
+`;
+
 const Section1 = styled.section`
   width: 100%;
+  height: 90%;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
-  gap: 10px;
-  margin: 0 0 20px 0;
+  gap: 50px;
 `;
 
 const BannerButton = styled.div`
   display: grid;
   place-items: center;
   width: 100%;
-  height: 200px;
-  border-radius: 5px;
-  border: 1px solid blue;
-`;
-
-const Section2 = styled.section`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  width: 100%;
-  gap: 10px;
-`;
-
-const Item = styled.div`
-  width: 100%;
-  border-radius: 5px;
-  border: 1px solid blue;
-  height: 50px;
-  display: grid;
-  place-items: center;
+  /* height: 200px; */
+  border-radius: 40px;
+  background: #bbd7e5;
+  color: white;
+  font-size: 50px;
+  font-weight: bolder;
 `;
 
 export default Home;
