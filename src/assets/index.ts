535a0ef2b180af1987fbbb4a1b9fdbbc914296ab
HwangJiseob <ghkdwltjq98@naver.com>
2021-05-24 14:45:00 +0900

    add: "@assets"
    add initial svg assets and made path alias "@assets"

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import { ReactComponent as ChatSVG } from "./chat.svg";
+import { ReactComponent as FilesSVG } from "./files.svg";
+import { ReactComponent as HomeSVG } from "./home.svg";
+import { ReactComponent as ManSVG } from "./man.svg";
+import { ReactComponent as SearchSVG } from "./search.svg";
+
+export { HomeSVG, ChatSVG, FilesSVG, ManSVG, SearchSVG };
+
+export default { HomeSVG, ChatSVG, FilesSVG, ManSVG, SearchSVG };
