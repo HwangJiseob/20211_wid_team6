874d86add82c0d99e5c58cf44ef3c3962baca5ce
HwangJiseob <ghkdwltjq98@naver.com>
2021-05-26 21:03:58 +0900

    add: Home.tsx pc version design
    add: Home.tsx pc version design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import styled from "@emotion/styled";
 import { useHistory } from "react-router-dom";
 
+import { layout } from "@libs/config";
+
+const { mobileBreakpoint } = layout;
+
 const Home = () => {
   const history = useHistory();
   return (
@@ -20,14 +24,38 @@ const Container = styled.div`
   display: grid;
   place-items: center;
   width: 100%;
-  height: calc(100vh - 150px); // 일단 하드코딩
+  height: calc(100vh - 275px);
+  /*
+    + 100px for header.pc_height
+    + 150px for footer.pc_height
+    +  25px for margins and paddings
+    ---------------------------------------
+    = 150px
+  */
+  ${mobileBreakpoint()} {
+    height: calc(100vh - 150px);
+    /*
+      + 60px for header.mobile_height
+      + 60px for naviator in mobile version
+      + 30px for margins and paddings
+      ---------------------------------------
+      = 150px
+     */
+  }
 `;
 
 const Section1 = styled.section`
   width: 100%;
   height: 90%;
   display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
   gap: 50px;
+  ${mobileBreakpoint()} {
+    width: 100%;
+    height: 90%;
+    display: grid;
+    gap: 50px;
+  }
 `;
 
 const BannerButton = styled.button`
