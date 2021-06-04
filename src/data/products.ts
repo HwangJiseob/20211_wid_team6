d30de50abc08caac382d46fd8a189772cbe9815c
HwangJiseob <ghkdwltjq98@naver.com>
2021-06-04 12:37:15 +0900

    add: product data
    add: product data

---
+++ src/data/products.ts
@@ -0,0 +1,72 @@
+export const products = [
+  {
+    name: "한송이",
+    path: "/products/0001",
+    limit: 1,
+    price: 3_000,
+  },
+  {
+    name: "S 꽃다발",
+    path: "/products/0002",
+    limit: 2,
+    price: 6_000,
+  },
+  {
+    name: "M 꽃다발",
+    path: "/products/0003",
+    limit: 8,
+    price: 20_000,
+  },
+  {
+    name: "L 꽃다발",
+    path: "/products/004",
+    limit: 15,
+    price: 35_000,
+  },
+  {
+    name: "XL 꽃다발",
+    path: "/products/005",
+    limit: 20,
+    price: 35_000,
+  },
+];
+
+export const flowers = [
+  {
+    name: "해바라기",
+    price: 2_500,
+  },
+  {
+    name: "리시안셔스",
+    price: 1_000,
+  },
+  {
+    name: "거베라",
+    price: 2_500,
+  },
+  {
+    name: "해바라기",
+    price: 2_000,
+  },
+  {
+    name: "러넌큘러스",
+    price: 1_500,
+  },
+  {
+    name: "델피늄",
+    price: 3_000,
+  },
+  {
+    name: "옥시",
+    price: 3_000,
+  },
+];
+
+export const editions = [
+  { name: "해바라기 정원", price: 50_000, dicsount: 10 },
+  { name: "작약", price: 25_900, dicsount: 10 },
+  { name: "파랑새", price: 56_900, dicsount: 0 },
+  { name: "코랄빛 카네이션", price: 56_900, dicsount: 0 },
+];
+
+export default flowers;
